2ef1354c3e725a4658fb8160261b6557594c0358
study4it <study4it@mail.ru>
2020-08-09 17:41:36 +0000

    Publish to GitHub Pages

--- docs/_next/static/manifest/_buildManifest.js
+++ docs/_next/static/manifest/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-ab6f5dbeb91c74d79159.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a98d98d178927e175cb7.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-98e414424de6d4d59c86.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e72dbcf7c523c34d863c.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- docs/_next/static/manifest/_ssgManifest.js
+++ docs/_next/static/manifest/_ssgManifest.js
@@ -1,1 +1,1 @@
-self.__SSG_MANIFEST=new Set(["\u002F"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
+self.__SSG_MANIFEST=new Set;self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
