715b3f7feca9d9da10b1798b4283c3d9ed0756d8
mrcheater <slayerx@narod.ru>
2020-08-09 08:40:32 +0300

    Setup next.js

---
+++ docs/_next/static/manifest/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-ab6f5dbeb91c74d79159.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a98d98d178927e175cb7.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Head from 'next/head'
+
+export async function getStaticProps() {
+  return {
+    props: {
+      something: ['one', 'two', 'three'],
+    },
+  }
+}
+
+export default function IndexPage({ something }) {
+  return (
+    <main>
+      <Head>
+        <title>Home page</title>
+      </Head>
+
+      <h1>List of something</h1>
+
+      <section>
+        {something.map((element, index) => (
+          <div key={index}>something - {element}</div>
+        ))}
+      </section>
+    </main>
+  )
+}
