69042a4aae577c2ba66449926e454e25879058ce
study4it <study4it@mail.ru>
2020-08-11 20:19:25 +0000

    Publish to GitHub Pages

--- docs/_next/static/manifest/_buildManifest.js
+++ docs/_next/static/manifest/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002F6bd7d69ebcdaace95d75.css","static\u002Fchunks\u002Fpages\u002Findex-a977a4ff347ff7f6b501.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e72dbcf7c523c34d863c.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002F6bd7d69ebcdaace95d75.css","static\u002Fchunks\u002Fpages\u002Findex-605e2adcb90c933a6899.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e72dbcf7c523c34d863c.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
